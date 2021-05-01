f58b20e7fec9f3bf5356342a2f426dbe53b2e9a7
ohjiwoo123 <80387186+ohjiwoo123@users.noreply.github.com>
2021-05-02 01:05:15 +0900

    Add files via upload

---
+++ backtest.py
@@ -0,0 +1,16 @@
+import pyupbit
+import numpy as np
+
+# OHCLV(open, high, low, close, volume)으로 당일 시가, 고가, 저가, 종가, 거래량에 대한 데이터 
+df = pyupbit.get_ohlcv("KRW-BTC", count=7)
+df['range'] = (df['high'] - df['low']) * 0.5
+df['target'] = df['open'] + df['range'].shift(1)
+
+df['ror'] = np.where(df['high'] > df['target'],
+                     df['close'] / df['target'] ,
+                     1)
+
+df['hpr'] = df['ror'].cumprod()
+df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+print("MDD(%): ", df['dd'].max())
+df.to_excel("dd.xlsx")
