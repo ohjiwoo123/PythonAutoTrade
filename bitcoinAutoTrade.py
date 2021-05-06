e9492c9e4fa133204f9b721f8411f45f69a10738
CodingBoy <80387186+ohjiwoo123@users.noreply.github.com>
2021-05-06 20:35:46 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "qFjzkora7QGNw9Am5oHOFb6URMV8gWxysyqZSgUw"
-secret = "Ze0aJH5USwkKxfmjnHnD9SKF6F13UTehLinAvvKS"
+access = "your-access-key"
+secret = "your-secret-key"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
